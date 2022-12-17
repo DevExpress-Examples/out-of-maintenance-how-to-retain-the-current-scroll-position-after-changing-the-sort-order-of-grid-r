c1c9379830bbdb733157bfbecc8297e81e3b4c56
DevExpressExampleBot <fakeEmail>
2022-12-17 11:27:37 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Controls.Primitives
 Imports System.Windows.Documents
@@ -7,53 +7,52 @@ Imports DevExpress.Xpf.Grid
 
 Namespace DXGridTest
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Private scrollInfo As IScrollInfo
-		Public Sub New()
-			InitializeComponent()
+        Private scrollInfo As IScrollInfo
 
-			Dim list As New List(Of TestData)()
-			For i As Integer = 0 To 999
-				list.Add(New TestData() With {
-					.Text = "Row" & i,
-					.Number = i,
-					.Group = i Mod 5
-				})
-			Next i
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As List(Of TestData) = New List(Of TestData)()
+            For i As Integer = 0 To 1000 - 1
+                list.Add(New TestData() With {.Text = "Row" & i, .Number = i, .Group = i Mod 5})
+            Next
 
-			grid.ItemsSource = list
+            Me.grid.ItemsSource = list
+            AddHandler Me.grid.Loaded, New RoutedEventHandler(AddressOf grid_Loaded)
+        End Sub
 
-			AddHandler grid.Loaded, AddressOf grid_Loaded
-		End Sub
+        Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            scrollInfo = GetScrollInfo()
+        End Sub
 
-		Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			scrollInfo = GetScrollInfo()
-		End Sub
-		Private Function GetScrollInfo() As IScrollInfo
-			Dim enumerator As New VisualTreeEnumerator(grid)
-			Do While enumerator.MoveNext()
-				If TypeOf enumerator.Current Is DataPresenter Then
-					Return TryCast(enumerator.Current, IScrollInfo)
-				End If
-			Loop
-			Return Nothing
-		End Function
+        Private Function GetScrollInfo() As IScrollInfo
+            Dim enumerator As VisualTreeEnumerator = New VisualTreeEnumerator(Me.grid)
+            While enumerator.MoveNext()
+                If TypeOf enumerator.Current Is DataPresenter Then Return TryCast(enumerator.Current, IScrollInfo)
+            End While
 
-		Private offset As Double
-		Private Sub grid_StartSorting(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			offset = scrollInfo.VerticalOffset
-		End Sub
+            Return Nothing
+        End Function
 
-		Private Sub grid_EndSorting(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			scrollInfo.SetVerticalOffset(offset)
-		End Sub
-	End Class
+        Private offset As Double
 
-	Public Class TestData
-		Public Property Text() As String
-		Public Property Number() As Integer
-		Public Property Group() As Integer
-	End Class
+        Private Sub grid_StartSorting(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            offset = scrollInfo.VerticalOffset
+        End Sub
+
+        Private Sub grid_EndSorting(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            scrollInfo.SetVerticalOffset(offset)
+        End Sub
+    End Class
+
+    Public Class TestData
+
+        Public Property Text As String
+
+        Public Property Number As Integer
+
+        Public Property Group As Integer
+    End Class
 End Namespace
